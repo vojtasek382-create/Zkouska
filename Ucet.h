f6c85b7359efddec698ebf63afe94a80c8479925
vojtasek382-create <vojtasek382@gmail.com>
2026-07-22 14:34:42 +0200

    Add files via upload

---
+++ Ucet.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+class Ucet {
+
+    protected:
+    std::string cisloUctu;
+    std::vector<double> historieTransakci;
+    static int pocetUctu;
+
+    public:
+    Ucet(std::string cisloUctu, std::vector<double> historieTransakci);
+    virtual ~Ucet();
+
+    static int getpocetUctu();
+    std::vector<double>& gethistorieTransakci();
+
+    void pridejTransakci(double hodnota);
+    void pridejTransakce(const std::vector<double>& hodnoty);
+
+    virtual void analyzujUcet() const;
+    virtual void vypisInfo() const;
+
+};
